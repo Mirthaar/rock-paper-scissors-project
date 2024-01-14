0e2deab03a3ab3d392b9b022236101dc685ea4fb
Mirthaar <154718513+Mirthaar@users.noreply.github.com>
2024-01-14 01:45:24 -0700

    fix: debug playRound function due to undefined

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,22 @@
 // Algorithm 
-
 // Goal: Create a Rock Paper Scissors game against a computer
-
 // Scope: Will need a player input and will need to return a result in an alert
 // A computer will randomly return rock, paper, or Scissors
 // The player can select from rock, paper, or Scissors
 // Define which selections win 
 
 // Pseudocode
-
-// Give player three options, rock, paper, scissor
-// Get text input from user and store it
-// Store the computer result
+// get Computer selection
+// get player Selection
 // Define the winning combintations
 // Compare the result of the player and the computer
 // Return result to the player
-
 // Run a randomizer for a computer that returns rock, paper, or scissor
+// Store the computer result
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice;
+
 function getComputerChoice() {
     let x =  Math.floor((Math.random() * 3) + 1);
     if (x === 1) {
@@ -29,6 +29,34 @@ function getComputerChoice() {
     return x;
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+        alert('Rock beats Scissors. You win!');
+    }
+}
+
+// function playRound(playerSelection, computerSelection) {
+//     if (playerSelection === 'rock' && computerSelection === 'Scissors') {
+//         alert('Rock beats Scissors. You win!'); 
+//     } else if (playerSelection === 'rock' && computerSelection === 'Paper') {
+//         alert('Paper beats Rock. You Lose!')
+//     } else if (playerSelection === 'paper' && computerSelection === 'Rock') {
+//         alert('Paper beats Rock. You win!'); 
+//     } else if (playerSelection === 'paper' && computerSelection === 'Scissors') {
+//         alert('Scissors beats Paper. You lose!');
+//     } else if (playerSelection === 'scissors' && computerSelection === 'Paper') {
+//         alert('Scissors beats Paper. You win!'); 
+//     } else if (playerSelection === 'scissors' && computerSelection === 'Rock') {
+//         alert('Rock beats Scissors. You lose!');
+//     } else if (playerSelection == computerSelection) {
+//         alert('It\'s a tie! Start over!');
+//     } else {
+//         alert('Hey Bitch It\'s Not Working')
+//     }}
+    
+
+console.log(computerSelection());
+console.log(playerSelection);
+console.log(playRound(playerSelection, computerSelection));
 
 
