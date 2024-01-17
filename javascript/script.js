5eaf98e5bfe2ed393d245feb88ae8bc649ca58b7
Mirthaar <154718513+Mirthaar@users.noreply.github.com>
2024-01-17 00:17:26 -0700

    feat: Solve logic for five rounds
    I wanted to test myself to include alerts and score keeping for the five rounds.
    I found this extremely challenging but the debugger in the devtools helped me step
    through each function to see what was going wrong.
    
    This challenge was incredibly fun and I cannot wait to build upon these skills for future,
    more difficult challenges.

--- javascript/script.js
+++ javascript/script.js
@@ -14,8 +14,56 @@
 // Run a randomizer for a computer that returns rock, paper, or scissor
 // Store the computer result
 
+// Assign starting score at beginning of game
+let playerScore = 0;
+let computerScore = 0;
+let draws = 0;
+let rounds = 0;
 
+// // Assign player and computer choices to new variables
+// let playerSelection = getPlayerChoice();
+// let computerSelection = getComputerChoice(); 
 
+// Logic for scoring and five rounds
+function game() {
+
+    while (rounds < 5) {
+        
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        let roundResult = playRound(playerSelection, computerSelection);
+        
+        if ((roundResult == 'You win! Paper beats Rock!') ||
+            (roundResult == 'You win! Rock beats Scissors!') ||
+            (roundResult == 'You win! Scissors beat Paper!')) {
+            playerScore++;
+        } else if ((roundResult == 'You lose. Scissors beat Paper.') ||
+            (roundResult == 'You lose. Paper beats Rock.') ||
+            (roundResult == 'You lose. Rock beats Scissors.')) {
+            computerScore++;
+            } else {
+            draws++;
+        } 
+        
+        rounds++;
+        alert(`Round: ${rounds}\n------------\nThe current score is:\nYou: ${playerScore}\nComputer: ${computerScore}`);
+        console.log(`Round: ${rounds}\n----\nThe current score is -\n You: ${playerScore}\nComputer: ${computerScore}`);
+        
+    }
+
+    if (playerScore > computerScore) {
+        alert(`Game Over! You win!\nFinal Score:\nYou: ${playerScore}\nComputer: ${computerScore}`);
+        console.log(`Game Over! You win!\nFinal Score:\nYou: ${playerScore}\nComputer: ${computerScore}`);
+    } else if (playerScore < computerScore) {
+        alert(`Game Over! You lose!\nFinal Score:\nYou: ${computerScore}\nComputer: ${playerScore}`);
+        console.log(`Game Over! You lose!\nFinal Score:\nYou: ${computerScore}\nComputer: ${playerScore}`);
+    } else if (playerScore === computerScore) {
+        alert(`Game Over! It's a tie!\nFinal Score:\nYou: ${playerScore}\nComputer: ${computerScore}`);
+        console.log(`Game Over! It's a tie!\nFinal Score:\nYou: ${playerScore}\nComputer: ${computerScore}`);
+    }
+}
+
+// Get a random choice from the computer
 function getComputerChoice() {
     let x =  Math.floor((Math.random() * 3) + 1);
     if (x === 1) {
@@ -39,18 +87,9 @@ function getPlayerChoice() {
         return playerChoice;
     } else {
         alert('Please select either Rock, Paper, or Scissors.');
-        return getPlayerChoice();
+        // return getPlayerChoice();
     } 
 }
-
-// Assign player and computer choices to new variables
-let playerSelection = getPlayerChoice();
-let computerSelection = getComputerChoice(); 
-
-// Assign starting score for beginning of game
-let playerScore = 0;
-let computerScore = 0;
-let draws = 0;
 
 //Compare choices and run the game
 function playRound(playerSelection, computerSelection) {
@@ -58,49 +97,39 @@ function playRound(playerSelection, computerSelection) {
     let response = '';
 
     if (playerSelection === 'paper' && computerSelection === 'rock'){
+        alert('You win! Paper Beats Rock!');
         return response += 'You win! Paper beats Rock!';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        alert('You win! Rock beats Scissors!');
         return response += 'You win! Rock beats Scissors!';
     } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        alert('You win! Scissors beat Paper!');
         return response += 'You win! Scissors beat Paper!';
     } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        alert('You lose. Scissors beat Paper!');
         return response += 'You lose. Scissors beat Paper.';
     } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        alert('You lose. Paper beats Rock.');
         return response += 'You lose. Paper beats Rock.';
     } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        alert('You lose. Rock beats Scissors.');
         return response += 'You lose. Rock beats Scissors.';
     } else if (playerSelection === computerSelection) {
+        alert('It\'s a tie.');
         return response += 'It\'s a tie.';
     } 
 }
 
-console.log(computerSelection);
-console.log(playerSelection);
-console.log(playRound(playerSelection, computerSelection));
+game();
 
-
-// Figure out game function here
-
-function game() {
-    //code goes here
-}
-
-// ----- Testing the logic to add one point if player wins
-let roundResult = playRound(playerSelection, computerSelection);
-if ((roundResult == 'You win! Paper beats Rock!') ||
-    (roundResult == 'You win! Rock beats Scissors!') ||
-    (roundResult == 'You win! Scissors beat Paper!')) {
-    playerScore++;
-} else if ((roundResult == 'You lose. Scissors beat Paper.') ||
-    (roundResult == 'You lose. Paper beats Rock.') ||
-    (roundResult == 'You lose. Rock beats Scissors.')) {
-    computerScore++;
-    } else {
-        draws++;
-    }
-// ----- End Testing
-
+// console.log(computerSelection);
+// console.log(playerSelection);
+// console.log(playRound(playerSelection, computerSelection))
 console.log(`Your current score is ${playerScore}.`);
 console.log(`The computer's current score is ${computerScore}.`);
-console.log(draws);
+console.log(`Current draws = ${draws}.`);
+console.log(`The current round is ${rounds}.`);
+
+
+
 
